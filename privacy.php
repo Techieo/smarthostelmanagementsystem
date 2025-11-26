54b51ea0ee941967a905d332025d67b6e87cd57b
Kihara Alex <nyathiru5395@gmail.com>
2025-11-26 12:56:35 +0300

    updated the privacy

--- privacy.php
+++ privacy.php
@@ -12,54 +12,54 @@
 <body>
 
     <!-- ===== NAVIGATION ===== -->
-    <header class="nav-header">
-        <nav class="navbar">
-            <div class="logo">
-                <img src="Img/favicon.jpg" alt="SHMS Logo">
-            </div>
-
-            <ul class="nav-links">
-                <li><a href="home_student.php">Home</a></li>
-                <li><a href="dashboard_student.php">Dashboard</a></li>
-                <li><a href="book_room.php">Rooms</a></li>
-                <li><a href="view_room_status_student.php">View room status</a></li>
-                <li><a href="submit_complaint_student.php">Submit Complaint</a></li>
-                <li><a href="payment_status_student.php">Payment Status</a></li>
-                <li class="dropdown">
-                    <a href="#">More <i class="fa-solid fa-caret-down"></i></a>
-                    <div class="dropdown-content">
-                        <a href="help_FAQs.php" style="color:black;">FAQ</a>
-                        <a href="rules_regulations.php" style="color:black;">Rules & Regulations</a>
-                        <a href="profile.php" style="color:black;">Profile</a>
-                        <a href="logout.php" style="color:black;">Log out</a>
-                    </div>
-                </li>
-            </ul>
-
-            <div class="icons">
-                <div class="hamburger" id="hamburger">
-                    <span></span><span></span><span></span>
-                </div>
-                <i class="fa-regular fa-bell"></i>
-            </div>
-        </nav>
-    </header>
-
-    <!-- ===== MOBILE MENU ===== -->
-    <div class="mobile-menu" id="mobileMenu">
-        <ul>
-            <li><a href="home_student.php">Home</a></li>
-            <li><a href="dashboard_student.php">Dashboard</a></li>
-            <li><a href="book_room.php">Rooms</a></li>
-            <li><a href="view_room_status_student.php">View room status</a></li>
-            <li><a href="submit_complaint_student.php">Submit Complaint</a></li>
-            <li><a href="payment_status_student.php">Payment Status</a></li>
-            <li><a href="help_FAQs.php">FAQs</a></li>
-            <li><a href="rules_regulations.php">Rules & Regulations</a></li>
-            <li><a href="profile_student.php">Profile</a></li>
-            <li><a href="logout.php">Log out</a></li>
-        </ul>
+    <header>
+  <nav>
+    <div class="logo">
+      <img src="Img/favicon.jpg" alt="SHMS Logo">
     </div>
+
+    <ul class="nav-links">
+      <li><a href="home_student.php">Home</a></li>
+      <li><a href="dashboard_student.php">Dashboard</a></li>
+      <li><a href="book_room.php">Rooms</a></li>
+      <li><a href="view_room_status_student.php">View room status</a></li>
+      <li><a href="submit_complaint_student.php">Submit Complaint</a></li>
+      <li><a href="payment_status_student.php">Payment Status</a></li>
+      <li class="dropdown">
+        <a href="#">More <i class="fa-solid fa-caret-down"></i></a>
+        <div class="dropdown-content">
+          <a href="help_FAQs.php">FAQ</a>
+          <a href="rules_regulations.php">Rules & Regulations</a>
+          <a href="profile.php">Profile</a>
+          <a href="logout.php">Log out</a>
+        </div>
+      </li>
+    </ul>
+
+    <div class="icons">
+      <div class="hamburger" id="hamburger">
+        <span></span><span></span><span></span>
+      </div>
+      <i class="fa-regular fa-bell"></i>
+    </div>
+  </nav>
+</header>
+
+<!-- =========== MOBILE MENU =========== -->
+<div class="mobile-menu" id="mobileMenu">
+  <ul>
+    <li><a href="home_student.php">Home</a></li>
+    <li><a href="dashboard_student.php">Dashboard</a></li>
+    <li><a href="book_room.php">Rooms</a></li>
+    <li><a href="view_room_status_student.php">View room status</a></li>
+    <li><a href="submit_complaint_student.php">Submit Complaint</a></li>
+    <li><a href="payment_status_student.php">Payment Status</a></li>
+    <li><a href="help_FAQs.php">FAQs</a></li>
+    <li><a href="rules_regulations.php">Rules & Regulations</a></li>
+    <li><a href="profile.php">Profile</a></li>
+    <li><a href="logout.php">Log out</a></li>
+  </ul>
+</div>
 
     <!-- ===== PRIVACY PAGE CONTENT ===== -->
     <section class="privacy-container">
