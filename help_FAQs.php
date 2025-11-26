7b75364bdff483d08bf07b50a03b853c85080374
Kihara Alex <nyathiru5395@gmail.com>
2025-11-26 14:21:51 +0300

    Update help_FAQs.php with latest dropdown changes

--- help_FAQs.php
+++ help_FAQs.php
@@ -8,7 +8,7 @@
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"  />
-      <script src="JAVASCRIPT_SHMS/FAQs_student.js" defer></script>
+      <script src="JAVASCRIPT_SHMS/rules_regulations_student.js" defer></script>
     <title>FAQs | Smart Hostel Management System</title>
   </head>
   <body>
