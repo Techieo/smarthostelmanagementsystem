042905c4cf4d7cb33bdc40f561de38cee377f212
Kihara Alex <nyathiru5395@gmail.com>
2025-11-27 03:23:55 +0300

    Updated home_student and replaced profile1 with profile.php

--- home_student.php
+++ home_student.php
@@ -41,7 +41,7 @@ while ($row = $result->fetch_assoc()) {
         <div class="dropdown-content">
           <a href="help_FAQs.php">FAQ</a>
           <a href="rules_regulations.php">Rules & Regulations</a>
-          <a href="profile.php">ProfileONE</a>
+          <a href="profile.php">Profile</a>
           <a href="logout.php">Log out</a>
         </div>
       </li>
