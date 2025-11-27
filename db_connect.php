cc9928c2a051f9f4b34bd8e25d045de1b6e21c04
Kihara Alex <nyathiru5395@gmail.com>
2025-11-27 04:00:12 +0300

    Update db_connect.php to handle localhost and hosted DB connections

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,21 @@
 <?php
-// Database configuration
-$hostname = "sql100.infinityfree.com";     // Server name
-$username = "if0_40512456";          // Database username
-$password = "JQHuidZzwpCqnVq";              // Database password
-$database = "if0_40512456_smart_db"; // Your database name
+// Detect environment
+$host = $_SERVER['HTTP_HOST'];
+
+// If running on localhost (XAMPP)
+if ($host === 'localhost' || $host === '127.0.0.1') {
+    $hostname = "localhost";      // Local database host
+    $username = "root";           // Local DB username
+    $password = "";               // Local DB password (XAMPP default empty)
+    $database = "smart_db";       // Your local database name
+
+} else {
+    // If running online (InfinityFree)
+    $hostname = "sql100.infinityfree.com";
+    $username = "if0_40512456";
+    $password = "JQHuidZzwpCqnVq";
+    $database = "if0_40512456_smart_db";
+}
 
 // Create connection
 $conn = new mysqli($hostname, $username, $password, $database);
