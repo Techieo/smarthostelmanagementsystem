aebf8393d058c4a74a9d51993bc55a06b421f5fe
Kihara Alex <nyathiru5395@gmail.com>
2025-11-27 10:41:32 +0300

    Update mpesa_config.php

--- mpesa_config.php
+++ mpesa_config.php
@@ -4,5 +4,5 @@ $consumerKey = "GVDWWYInK9uhW7UFpGGco0z8ZnUYexAJuF6SdyZFnsJAWDl0";
 $consumerSecret = "UD0OoGyYso8Ed3sccAK2sh0AG2u3kWjG7FqDI35FqMZARGN59cl2tsehGDx34hno";
 $shortCode = "174379";  // sandbox shortcode
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-<?php if (!empty($success_message)) echo "<div class='success-message'>{$success_message}</div>"; ?>
+$callbackURL = "https://hostelmanagementsystem.wuaze.com/stk_callback.php";
 ?>
